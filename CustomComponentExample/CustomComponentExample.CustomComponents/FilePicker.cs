fd00542901da21e2b040df5dd17c6404821c0cae
Vlad Golovin <localuser@gmail.com>
2018-02-16 20:06:42 +0500

    Добавлены UI тесты.

---
+++ CustomComponentExample/CustomComponentExample.CustomComponents/FilePicker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomComponentExample.CustomComponents
+{
+    public partial class FilePicker : UserControl
+    {
+        public string FilePath
+        {
+            get
+            {
+                return FilePathText.Text;
+            }
+            set
+            {
+                FilePathText.Text = value;
+            }
+        }
+        public FilePicker()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FilePathText.Text = openFileDialog1.FileName;
+            }
+            
+        }
+    }
+}
---
+++ CustomComponentExample/CustomComponentExample.CustomComponents/NumberTextbox.cs
@@ -0,0 +1,37 @@
+﻿namespace CustomComponentExample.CustomComponents
+{    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public partial class NumberTextbox : TextBox
+    {
+        public NumberTextbox()
+        {
+            InitializeComponent();
+        }
+
+        public NumberTextbox(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+
+            int n = 0;
+
+            if (int.TryParse(Text, out n))
+                ForeColor = System.Drawing.Color.Black;
+            else
+                ForeColor = System.Drawing.Color.Red;
+        }
+    }
+}
